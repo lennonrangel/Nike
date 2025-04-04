3168d1c0dcd93b68a6b737878ec9a0ea0479bb3c
lennonrangel <lennonpereirarangel@hotmail.com>
2025-04-04 20:34:07 -0300

    Criação das interações principais

---
+++ script.js
@@ -0,0 +1,7 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector(".imagem-tenis")
+
+function mudarVisual(cor, imagem){
+    body.style.background = cor
+    tenis.src = imagem
+}
